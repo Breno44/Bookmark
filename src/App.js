c60c7b1bbb9a353a8f4bd5fdd387b9272c5377b3
Breno <bhfs.2908@gmail.com>
2021-10-15 15:14:35 -0300

    feat:add download section

--- src/App.js
+++ src/App.js
@@ -124,6 +124,63 @@ function App() {
           <div className="hidden lg:block overflow-hidden bg-bookmark-purple rounded-r-full absolute h-80 w-2/4 -bottom-24 -left-36"></div>
         </div>
       </section>
+
+      <section className="py-20 mt-20">
+        <div className="sm:w-3/4 lg:w-5/12 mx-auto px-2">
+          <h1 className="text-3xl text-center text-bookmark-blue">Download the extension</h1>
+          <p className="text-center text-bookmark-grey mt-4">
+            We’ve got more browsers in the pipeline. Please do let us know if you’ve got a favourite you’d like us to
+            prioritize.
+          </p>
+        </div>
+        <div className="container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-16 max-w-screen-lg mt-16 lg:mb-16">
+          <div className="flex flex-col rounded-md shadow-md">
+            <div className="p-6 flex flex-col items-center">
+              <img src="/imgs/logo-chrome.svg" alt="" />
+              <h3 className="mt-5 mb-2 text-bookmark-blue text-lg">Add to chrome</h3>
+              <p className="mb-2 text-bookmark-grey font-light">Minimum version 62</p>
+            </div>
+            <hr className="border-b border-bookmark-white" />
+            <div className="flex p-6">
+              <button type="button" className="flex-1 btn btn-purple hover:bg-bookmark-white hover:text-black">
+                Add & Install Extension
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-16 max-w-screen-lg mt-16 lg:my-8">
+          <div className="flex flex-col rounded-md shadow-md">
+            <div className="p-6 flex flex-col items-center">
+              <img src="/imgs/logo-firefox.svg" alt="" />
+              <h3 className="mt-5 mb-2 text-bookmark-blue text-lg">Add to Firefox</h3>
+              <p className="mb-2 text-bookmark-grey font-light">Minimum version 62</p>
+            </div>
+            <hr className="border-b border-bookmark-white" />
+            <div className="flex p-6">
+              <button type="button" className="flex-1 btn btn-purple hover:bg-bookmark-white hover:text-black">
+                Add & Install Extension
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-16 max-w-screen-lg mt-16 lg:mt-16">
+          <div className="flex flex-col rounded-md shadow-md">
+            <div className="p-6 flex flex-col items-center">
+              <img src="/imgs/logo-opera.svg" alt="" />
+              <h3 className="mt-5 mb-2 text-bookmark-blue text-lg">Add to chrome</h3>
+              <p className="mb-2 text-bookmark-grey font-light">Minimum version 62</p>
+            </div>
+            <hr className="border-b border-bookmark-white" />
+            <div className="flex p-6">
+              <button type="button" className="flex-1 btn btn-purple hover:bg-bookmark-white hover:text-black">
+                Add & Install Extension
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section></section>
     </>
   );
 }
